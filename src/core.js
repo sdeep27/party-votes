e6da530a8ac3c6a93c294f95ec992d00c2c8dc8d
Deep Pulusani <sdeep27@yahoo.com>
2017-05-08 13:59:46 -0700

    finished basic core functions and corresponding tests

--- src/core.js
+++ src/core.js
@@ -4,10 +4,27 @@ export function setEntries(state, entries) {
   return state.set('entries', List(entries));
 };
 
+function getWinners(vote){
+  if (!vote) return [];
+  const [a,b] = vote.get('pair');
+  const aTally = vote.getIn(['tally', a], 0);
+  const bTally = vote.getIn(['tally', b], 0);
+  if (aTally > bTally) return [a];
+  else if (bTally > aTally) return [b];
+  else return [a, b];
+}
+
 export function next(state) {
-  const entries = state.get('entries');
-  return state.merge({
+  const entries = state.get('entries').concat(getWinners(state.get('vote')));
+  if (entries.size === 1) {
+    return state.remove('vote').remove('entries').set('winner', entries.first());
+  }
+  else return state.merge({
     vote: Map({pair: entries.take(2)}),
     entries: entries.skip(2)
   });
 };
+
+export function vote(state, item) {
+  return state.updateIn(['vote', 'tally', item], (item = 0) => ++item);
+}
--- test/core_spec.js
+++ test/core_spec.js
@@ -1,6 +1,6 @@
 import {expect} from 'chai';
 import {List, Map} from 'immutable';
-import {setEntries, next} from '../src/core';
+import {setEntries, next, vote} from '../src/core';
 
 describe('core functions', () => {
   describe('setEntries', () => {
@@ -14,14 +14,112 @@ describe('core functions', () => {
     });
   });
   describe('next', () => {
-    const state = Map({entries: List.of('Interstellar', 'Dark Knight', 'Mary Jane')})
     it('should create new pair and modify entries', () => {
+      const state = Map({entries: List.of('Interstellar', 'Dark Knight', 'Mary Jane')})
       const nextState = next(state);
       expect(nextState).to.equal(Map({
         vote: Map({
           pair: List.of('Interstellar', 'Dark Knight')
         }),
         entries: List.of('Mary Jane')
+      }));
+    });
+    it('should move winner to entries and update pair', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Interstellar', 'Dark Knight'),
+          tally: Map({
+            'Interstellar': 4,
+            'Dark Knight': 2
+          })
+        }),
+        entries: List.of('Mary Jane', 'The Prestige')  
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Mary Jane', 'The Prestige'),
+        }),
+        entries: List.of('Interstellar')  
+      })); 
+    });
+    it('should move pair in a draw to entries', () => {
+      const state = Map({
+        vote: Map({
+          pair: List(['Interstellar', 'Dark Knight']),
+          tally: Map({
+            'Interstellar': 3,
+            'Dark Knight': 3
+          })
+        }),
+        entries: List(['Mary Jane', 'The Prestige'])
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List(['Mary Jane', 'The Prestige']),
+        }),
+        entries: List(['Interstellar', 'Dark Knight'])
+      }));
+    });
+    it('should create a winner when nothing left to vote on', () => {
+      const state = Map({
+        vote: Map({
+          pair: List(['Interstellar', 'The Prestige']),
+          tally: Map({
+            'Interstellar': 2,
+            'The Prestige': 4
+          })
+        }),
+        entries: List()
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        winner: 'The Prestige'
+      }));
+    });
+  });
+  describe('vote', () => {
+    const state = Map({
+      vote: Map({
+        pair: List.of('Interstellar, Dark Knight'),
+        tally: Map()
+      }),
+      entries: List.of('Mary Jane')
+    })
+    it('should increment tally when 0', () => {
+      const nextState = vote(state, 'Interstellar');
+      expect(nextState).to.equal(Map({
+        vote: Map({
+        pair: List.of('Interstellar, Dark Knight'),
+        tally: Map({
+          'Interstellar': 1
+        })
+      }),
+        entries: List.of('Mary Jane')
+      }));
+    });
+    it('should increment when tally exists', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Interstellar', 'Dark Knight'),
+          tally: Map({
+            'Interstellar': 3,
+            'Dark Knight': 2
+          })
+        }),
+        entries: List()
+      })
+      const nextState = vote(state, 'Dark Knight');
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Interstellar', 'Dark Knight'),
+          tally: Map({
+            'Interstellar': 3,
+            'Dark Knight': 3
+          })
+        }),
+        entries: List()
       }));
     });
   });
