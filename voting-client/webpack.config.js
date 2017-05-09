4eb833f99a7d6bc70388eb8cf5fe9b58c68cc91b
Deep Pulusani <sdeep27@yahoo.com>
2017-05-09 11:24:37 -0700

    added client and loaded webpack and testing

---
+++ voting-client/webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack'); //needed for the hot loading plugin
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080', //client-side library of webpack dev server
+    'webpack/hot/only-dev-server', //webpack hot module loader
+    './src/index.js'
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
+  resolve: {
+    extensions: ['*','.js','.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
